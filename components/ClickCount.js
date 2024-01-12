dec7cca7e5ac2005432bc12cbf493260b5d3461d
Alejandro Oviedo Cruz <53951218+aloviedo@users.noreply.github.com>
2024-01-12 11:39:04 -0800

    Fix #4: Add a button to reset state on main page

--- components/ClickCount.js
+++ components/ClickCount.js
@@ -1,11 +1,23 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useState, useContext } from 'react'
 import Button from './Button'
 
-export default function ClickCount() {
-  const [count, setCount] = useState(0)
-  const increment = useCallback(() => {
-    setCount((v) => v + 1)
-  }, [setCount])
+// Import useContext from react
+import { ClickContext } from '../pages/index'
 
-  return <Button onClick={increment}>Clicks: {count}</Button>
+export default function ClickCount() {
+  const [clicks, setClicks] = useState(0)
+  const increment = useCallback(() => {
+    setClicks((v) => v + 1)
+  }, [setClicks])
+
+  // Use useContext to access the resetState function from ClickContext
+  const resetState = useContext(ClickContext)
+
+  // Modify the resetState function to also set the state of clicks to zero
+  const resetClicks = useCallback(() => {
+    resetState()
+    setClicks(0)
+  }, [resetState, setClicks])
+
+  return <Button onClick={increment}>Clicks: {clicks}</Button>
 }
